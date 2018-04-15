e566f69739ee9dd4a6aa6b3f9db49ad2b03f1a6a
David Alber <alber.david@gmail.com>
2018-04-14 21:31:37 -0700

    Moving config creation to method

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,21 @@ struct Config {
     filename: String,
 }
 
-fn parse_config() -> Config {
-    let args: Vec<String> = env::args().skip(1).collect();
-    if args.len() != 2 {
-        panic!("Two arguments are required: query and file");
-    }
+impl Config {
+    fn from_args() -> Config {
+        let args: Vec<String> = env::args().skip(1).collect();
+        if args.len() != 2 {
+            panic!("Two arguments are required: query and file");
+        }
 
-    let query = args[0].clone();
-    let filename = args[1].clone();
-    Config { query, filename }
+        let query = args[0].clone();
+        let filename = args[1].clone();
+        Config { query, filename }
+    }
 }
 
 fn main() {
-    let config = parse_config();
+    let config = Config::from_args();
     let mut f = File::open(config.filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
