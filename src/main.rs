44e593a20bd35fe672c632bb2d202d9997b2dad9
David Alber <alber.david@gmail.com>
2018-04-14 21:28:56 -0700

    Creating Config struct

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,25 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn parse_config() -> (String, String) {
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config() -> Config {
     let args: Vec<String> = env::args().skip(1).collect();
     if args.len() != 2 {
         panic!("Two arguments are required: query and file");
     }
 
-    let query = args[0].to_owned();
-    let filename = args[1].to_owned();
-    (query, filename)
+    let query = args[0].clone();
+    let filename = args[1].clone();
+    Config { query, filename }
 }
 
 fn main() {
-    let (query, filename) = parse_config();
-    let mut f = File::open(filename).expect("file not found");
+    let config = parse_config();
+    let mut f = File::open(config.filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
 }
