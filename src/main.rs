9440bbad0d0de7d3cc6d540fa34ff243bc661672
David Alber <alber.david@gmail.com>
2018-04-14 21:20:13 -0700

    Reading file contents

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
@@ -7,8 +9,11 @@ fn main() {
     }
 
     let query = &args[0];
-    let file = &args[1];
+    let filename = &args[1];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file);
+    let mut f = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+
+    println!("{}", contents);
 }
