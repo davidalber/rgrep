201db5a4a872fbc8b45776884865cb5b3cf58018
David Alber <alber.david@gmail.com>
2018-04-14 21:48:16 -0700

    Returning Result from Config constructor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 struct Config {
     query: String,
@@ -8,20 +9,23 @@ struct Config {
 }
 
 impl Config {
-    fn from_args() -> Config {
+    fn from_args() -> Result<Config, &'static str> {
         let args: Vec<String> = env::args().skip(1).collect();
         if args.len() != 2 {
-            panic!("Two arguments are required: query and file");
+            return Err("two arguments are required (query and file)");
         }
 
         let query = args[0].clone();
         let filename = args[1].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
 
 fn main() {
-    let config = Config::from_args();
+    let config = Config::from_args().unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     let mut f = File::open(config.filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
