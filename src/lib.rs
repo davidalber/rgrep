ee630c42f5e069db99f78d88b4a13353b7d1f040
David Alber <alber.david@gmail.com>
2018-04-14 22:11:34 -0700

    Moving logic into library

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn from_args() -> Result<Config, &'static str> {
+        let args: Vec<String> = env::args().skip(1).collect();
+        if args.len() != 2 {
+            return Err("two arguments are required (query and file)");
+        }
+
+        let query = args[0].clone();
+        let filename = args[1].clone();
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,8 @@
-use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
+extern crate rgrep;
+
 use std::process;
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn from_args() -> Result<Config, &'static str> {
-        let args: Vec<String> = env::args().skip(1).collect();
-        if args.len() != 2 {
-            return Err("two arguments are required (query and file)");
-        }
-
-        let query = args[0].clone();
-        let filename = args[1].clone();
-        Ok(Config { query, filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename)?;
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-    Ok(())
-}
+use rgrep::Config;
 
 fn main() {
     let config = Config::from_args().unwrap_or_else(|err| {
@@ -35,7 +10,7 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(err) = run(config){
+    if let Err(err) = rgrep::run(config){
         println!("Application error: {}", err);
         process::exit(1);
     };
