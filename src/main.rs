937231affc53e042bf4ef929cfb21c254f030e1f
David Alber <alber.david@gmail.com>
2018-04-14 21:25:01 -0700

    Extracting configuration parsing

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn main() {
+fn parse_config() -> (String, String) {
     let args: Vec<String> = env::args().skip(1).collect();
     if args.len() != 2 {
         panic!("Two arguments are required: query and file");
     }
 
-    let query = &args[0];
-    let filename = &args[1];
+    let query = args[0].to_owned();
+    let filename = args[1].to_owned();
+    (query, filename)
+}
 
+fn main() {
+    let (query, filename) = parse_config();
     let mut f = File::open(filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
-
-    println!("{}", contents);
 }
