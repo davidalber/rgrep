f960cbe6dc8a22fa1ce879b4cf55827715307ebe
David Alber <alber.david@gmail.com>
2018-07-27 22:23:07 -0700

    Add simple code on Box type

---
+++ box_type/src/main.rs
@@ -0,0 +1,12 @@
+// See https://doc.rust-lang.org/book/second-edition/ch15-01-box.html.
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
