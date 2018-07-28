537db753fa6ddb5a6c404f9240fe19a811b54911
David Alber <alber.david@gmail.com>
2018-07-28 01:07:23 -0700

    Add simple Deref code

---
+++ deref/src/main.rs
@@ -0,0 +1,34 @@
+// See https://doc.rust-lang.org/book/second-edition/ch15-02-deref.html.
+
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let yy = MyBox::new(String::from("hi"));
+    hello(&yy);
+}
