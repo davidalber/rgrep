163db88e83a03c7ed1ede9db2441fdf2467617ef
David Alber <alber.david@gmail.com>
2018-08-11 21:38:43 -0700

    Add thread example

---
+++ threads/src/main.rs
@@ -0,0 +1,20 @@
+// Basic thread code from https://doc.rust-lang.org/book/second-edition/ch16-01-threads.html.
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+}
