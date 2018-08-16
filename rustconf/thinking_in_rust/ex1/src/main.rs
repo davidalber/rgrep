d8b81a1aac7128a8cc6b42a292fc1c20756d5bf3
David Alber <alber.david@gmail.com>
2018-08-16 14:02:17 -0700

    Rustified

--- rustconf/thinking_in_rust/ex1/src/main.rs
+++ rustconf/thinking_in_rust/ex1/src/main.rs
@@ -1,20 +1,9 @@
 fn foo(input: Option<i32>) -> Option<i32> {
-    if input.is_none() {
-        return None;
-    }
-
-    let input = input.unwrap();
-    if input < 0 {
-        return None;
-    }
-    Some(input)
+    input.filter(|i| i >= &0)
 }
 
 fn bar(input: Option<i32>) -> Result<i32, ErrNegative> {
-    match foo(input) {
-        Some(n) => Ok(n),
-        None => Err(ErrNegative),
-    }
+    input.ok_or(ErrNegative)
 }
 
 #[derive(Debug)]
