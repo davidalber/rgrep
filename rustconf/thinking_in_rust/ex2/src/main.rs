4d11e157936726735605a4ccc01a238d776b5653
David Alber <alber.david@gmail.com>
2018-08-16 14:18:39 -0700

    Make more primitive

--- rustconf/thinking_in_rust/ex2/src/main.rs
+++ rustconf/thinking_in_rust/ex2/src/main.rs
@@ -2,7 +2,10 @@ fn main() {
     let vec = vec![0, 1, 2, 3];
 
     let mut iter = (&vec).into_iter();
-    while let Some(v) = iter.next() {
-        println!("{}", v);
+    loop {
+        match iter.next() {
+            Some(v) => println!("{}", v),
+            None => break,
+        }
     }
 }
