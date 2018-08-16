f9e1e98b6e2ef303e22071b14ec2dc5abe156d30
David Alber <alber.david@gmail.com>
2018-08-16 14:01:59 -0700

    Initial state

---
+++ rustconf/thinking_in_rust/ex1/src/main.rs
@@ -0,0 +1,28 @@
+fn foo(input: Option<i32>) -> Option<i32> {
+    if input.is_none() {
+        return None;
+    }
+
+    let input = input.unwrap();
+    if input < 0 {
+        return None;
+    }
+    Some(input)
+}
+
+fn bar(input: Option<i32>) -> Result<i32, ErrNegative> {
+    match foo(input) {
+        Some(n) => Ok(n),
+        None => Err(ErrNegative),
+    }
+}
+
+#[derive(Debug)]
+struct ErrNegative;
+
+fn main() {
+    println!("{:?}", foo(Some(0)));
+    println!("{:?}", foo(None));
+    println!("{:?}", bar(Some(42)));
+    println!("{:?}", bar(None));
+}
