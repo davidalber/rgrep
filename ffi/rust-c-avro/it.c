bdd43e21ce83b0502bc370db840683af7be031bc
David Alber <david.alber@redfin.com>
2018-09-05 08:57:02 -0700

    Add tracing print statements

--- ffi/rust-c-avro/it.c
+++ ffi/rust-c-avro/it.c
@@ -4,10 +4,11 @@
 char *hello_rust();
 
 void call_me_back(char *msg) {
-    printf("%s\n", msg);
+    printf("[C] Passed by Rust to `call_me_back`: %s\n", msg);
 }
 
 int main() {
-    printf("%s\n", hello_rust());
+    printf("[C] Calling `hello_rust`\n");
+    printf("[C] Returned by Rust: %s\n", hello_rust());
     return 0;
 }
--- ffi/rust-c-avro/src/lib.rs
+++ ffi/rust-c-avro/src/lib.rs
@@ -4,6 +4,8 @@ extern {
 
 #[no_mangle]
 pub extern fn hello_rust() -> *const u8 {
+    println!("[Rust] `hello_rust` called by C");
+    println!("[Rust] Calling `call_me_back`");
     unsafe { call_me_back("Hey\0".as_ptr()); }
     "Hello, world! 🐸\0".as_ptr()
 }
