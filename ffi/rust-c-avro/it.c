8602de552ab476d8d5ffe2bfe38d678cdad7f226
David Alber <david.alber@redfin.com>
2018-09-05 08:51:57 -0700

    Add call to C from Rust

--- ffi/rust-c-avro/it.c
+++ ffi/rust-c-avro/it.c
@@ -3,6 +3,10 @@
 
 char *hello_rust();
 
+void call_me_back(char *msg) {
+    printf("%s\n", msg);
+}
+
 int main() {
     printf("%s\n", hello_rust());
     return 0;
--- ffi/rust-c-avro/src/lib.rs
+++ ffi/rust-c-avro/src/lib.rs
@@ -1,4 +1,9 @@
+extern {
+    fn call_me_back(msg: *const u8);
+}
+
 #[no_mangle]
 pub extern fn hello_rust() -> *const u8 {
+    unsafe { call_me_back("Hey\0".as_ptr()); }
     "Hello, world! 🐸\0".as_ptr()
 }
